099f9dd8c1c8952cb4e4c3a8fd071531de2c7626
James Groves <triskept@gmail.com>
2019-10-29 10:45:35 -0400

    back-end set up

--- AlbumProject/Data/ArtistContext.cs
+++ AlbumProject/Data/ArtistContext.cs
@@ -1,11 +1,44 @@
-﻿using System;
+﻿using AlbumProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AlbumProject.Data
 {
-    public class ArtistContext
+    public class ArtistContext : DbContext
     {
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Song> Songs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=ArtistDB;Trusted_Connection=True;";
+
+            optionsBuilder.UseSqlServer(connectionString)
+            .UseLazyLoadingProxies();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Album>().HasData(
+                new Album()
+                {
+                });
+
+            modelBuilder.Entity<Artist>().HasData(
+                new Artist()
+                {
+                });
+
+            modelBuilder.Entity<Song>().HasData(
+                new Song()
+                {
+                });
+
+        }
     }
 }
--- AlbumProject/Startup.cs
+++ AlbumProject/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AlbumProject.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +16,8 @@ namespace AlbumProject
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddDbContext<ArtistContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,10 +27,18 @@ namespace AlbumProject
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
