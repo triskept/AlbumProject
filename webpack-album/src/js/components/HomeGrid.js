301c45de25acfc6c7649d7413164c5b076b9117c
James Groves <triskept@gmail.com>
2019-10-30 14:26:36 -0400

    added img folder to front end

--- webpack-album/src/js/components/HomeGrid.js
+++ webpack-album/src/js/components/HomeGrid.js
@@ -3,15 +3,15 @@ export default function HomeGrid(){
     <section id="grid">
         <div id="artists">
             <h3>Artists</h3>
-            <img src="/images/singer.jpg"/>
+            <img src="/images/singer.jpg"></img>
         </div>
         <div id="albums">
             <h3>Albums</h3>
-            <img src="/images/record.png">
+            <img src="/images/record.png"></img>
         </div>
         <div id="songs">
             <h3>Songs</h3>
-            <img src="/images/music.png">
+            <img src="/images/music.png"></img>
         </div>
     </section>
 `
