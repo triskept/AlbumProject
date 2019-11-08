d6927c8acf3bffd4fb702c6b22f90fe9ede5102e
Jeremy <JeremyMinnick2019@Gmail.com>
2019-11-08 10:38:36 -0500

    entity changed in IRepos

--- AlbumProject/Repositories/IRepository.cs
+++ AlbumProject/Repositories/IRepository.cs
@@ -11,9 +11,8 @@ namespace AlbumProject.Repositories
     {
         IEnumerable<T> GetAll();
         T GetById(int id);
-        void Create(T artist);
-        void Delete(T artist);
-        void Update(T artist);
-        void Save();
+        void Create(T entity);
+        void Delete(T entity);
+        void Update(T entity);
     }
 }
